9f139b3f0208e65907b4adfc1cdc25b643000b9b
Aleksey Shurygin <magnito12@yandex.ru>
2017-07-19 22:33:51 +0700

    Working good, but there is no defence

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 $(document).ready(function(){
     let deleteDate, length;
     let fileDeleteId=[];
-    let indexDeleteFile=[];
-    let IDarrForDeleting=[];
+    let fileIDMatrix=[];
+    let fileDelRow=[];
+    let fileRowLength=0;
     let entityID=[];
-    let foldersID=[];      
+    let foldersID=[]; 
+    const matrixLength=80;
     let styles={visibility:"visible",position:"absolute",top:"25px",left:"600px"};
     let foldersFound = new Event("click");
     let entetyesFound = new Event("dblclick");
+    let filesFound = new Event("click");
     var t=0; // счетчик для хранилищ данных
     let l=0; // счетчик для папок
+    let f=0; // счетчик для файлов
     
     
     $('#find-button').click(function(){
@@ -31,8 +35,8 @@ $(document).ready(function(){
                     if(result.more()){
                         result.next();
                     } else{
-                        $('#entetyes-id').html('Найдено '+entityID.length+' хранилищ данных');
-                        $('#folders-id').html('Идет поиск папок');
+                        $('#entetyes-id').html('Хранилищ данных найдено '+entityID.length);
+                        $('#folders-id').html('Идет поиск папок...');
                         elem.dispatchEvent(entetyesFound);
                     }
                 }
@@ -44,107 +48,153 @@ $(document).ready(function(){
     $('#elem').dblclick(function(){       
         if(entityID[t]!=undefined){
                 BX24.callMethod("disk.storage.getchildren",{id: entityID[t], filter: {"<CREATE_TIME": deleteDate}},function (result){                                    
-			             if (result.error()){                             
-                                console.log("!!!!ID of FAILED entety is:  "+entityID[t]);                                
+			             if (result.error()){ 
+                            t++;
+                            console.log("!!!!ID of FAILED entety is:  "+entityID[t]);
+                             setTimeout(function(){
+                                $('#folders-id').html('Идет поиск папок... найдено: '+foldersID.length+" хранилищ проверено: "+(t+1));                                
+                                elem.dispatchEvent(entetyesFound);                                    
+                            },100); 
                                 elem.dispatchEvent(entetyesFound);                                                        
                          } else{
+                            if(result.more()){
+                                result.data().forEach(function(folder, i){
+                                    foldersID.push(folder.ID);
+                                });
+                                setTimeout(function(){                                    
+                                    result.next();
+                                    $('#folders-id').html('Идет поиск папок... найдено: '+foldersID.length+" хранилищ проверено: "+(t+1));
+                                },1000); 
+                            }    
                             if(result.data().length==0){
                                 setTimeout(function(){
-                                    console.log("ID of checked entety is: "+entityID[t]);
+                                    $('#folders-id').html('Идет поиск папок... найдено: '+foldersID.length+" хранилищ проверено: "+(t+1));
                                     t++;
                                     elem.dispatchEvent(entetyesFound);
                                 }, 250);                                
-                            }
-                            result.data().forEach(function(folder, i){ 
-                                foldersID.push(folder.ID);                                
-                                length=result.data().length-1;                                
-                                if (i==length){
-                                    setTimeout(function(){
-                                        console.log("ID of checked entety is: "+entityID[t]);
-                                        t++;
-                                        elem.dispatchEvent(entetyesFound);
-                                    }, 250);                                    
-                                }
-                            });                            
-                            if(result.more()){
-                                result.next();
-                            }                               
+                            } else {
+                                result.data().forEach(function(folder, i){ 
+                                    foldersID.push(folder.ID);                                
+                                    length=result.data().length-1;                                
+                                    if (i==length){
+                                        setTimeout(function(){
+                                            $('#folders-id').html('Идет поиск папок... найдено: '+foldersID.length+" хранилищ проверено: "+(t+1));
+                                            t++;
+                                            elem.dispatchEvent(entetyesFound);
+                                        },250);                                    
+                                    }
+                                }); 
+                            }                         
                         }
-		              });
-             console.log(entityID[t]+" checking...");
+		              });             
         } else{
-            $('#folders-id').html('Найдено '+foldersID.length+' папок');
+            $('#folders-id').html('Папок найдено: '+foldersID.length);
             elem.dispatchEvent(foldersFound);           
         }        
     });
        
     /*Поиск всех файлов на диске соответствующих фильтру и сохрание их ID в список*/
-    
+  
     $('#elem').click(function(){
         if (foldersID[l]!=undefined){
             BX24.callMethod("disk.folder.getchildren",{id: foldersID[l],filter: {"<UPDATE_TIME": deleteDate}},function (result){
-			             if (result.error()){
-                             console.error(result.error());
+			             if (result.error()){                             
+                             l++;
                              setTimeout(function(){
-                                 elem.dispatchEvent(foldersFound);
-                             },1000);
+                                $('#delete-arr-length').html('Идет поиск файлов... найдено: '+fileDeleteId.length+" папок проверено: "+(l+1));                                
+                                elem.dispatchEvent(foldersFound);                                    
+                             },100); 
                          } else{
-                            var files=[];
+                            if(result.more()){
                                 result.data().forEach(function(file, i){ 
-                                    files.push(file.ID);
                                     fileDeleteId.push(file.ID);
                                 });
-                                if(result.more()){
-                                    result.next();
-                                } else {
-                                    console.log("Files in folder: "+foldersID[l]+"\n"+files);
+                                setTimeout(function(){
+                                    $('#delete-arr-length').html('Идет поиск файлов... найдено: '+fileDeleteId.length+" папок проверено: "+(l+1));
+                                    result.next();                                    
+                                },1000);                                
+                            } else if(result.data().length==0){
+                                setTimeout(function(){
+                                    $('#delete-arr-length').html('Идет поиск файлов... найдено: '+fileDeleteId.length+" папок проверено: "+(l+1));
                                     l++;
                                     elem.dispatchEvent(foldersFound);
-                                }
+                                }, 250);                                
+                            } else {
+                                result.data().forEach(function(file, i){ 
+                                    fileDeleteId.push(file.ID);                                
+                                    length=result.data().length-1;                                
+                                    if (i==length){
+                                        setTimeout(function(){
+                                            $('#delete-arr-length').html('Идет поиск файлов... найдено: '+fileDeleteId.length+" папок проверено: "+(l+1));                                            
+                                            l++;
+                                            elem.dispatchEvent(foldersFound);
+                                        }, 1000);                                    
+                                    }
+                                });
+                            }
+                             
                         }
 		              });
-        } else {
-                $('#delete-arr-length').html('Вы можете удалить '+fileDeleteId.length+" файлов."+" Удаление файлов займет около: "+Math.floor(fileDeleteId.length/120)+" минут");
-                $('#loading-pic').css({visibility:"hidden"});
-                $('#delete-ready-info').html('');
-                clearInterval(searchingFiles);
+        } else {                
+            $('#delete-arr-length').html('Вы можете удалить '+fileDeleteId.length+" файлов."+" Удаление файлов займет около: "+Math.floor(fileDeleteId.length/120)+" минут");
+            fileIDMatrix=MakeItMatrix(fileDeleteId,matrixLength);
+            $('#loading-pic').css({visibility:"hidden"});
+            $('#delete-ready-info').html('');
+            del.dispatchEvent(filesFound);
         }
-    });    
-    
-    
-    
-    $('#delete-button').click(function(){
-        $('#loading-pic').css(styles);        
-        let i=0; 
-        let filesDeleted=0;
-        let iID=setInterval(function (){
-            IDarrForDeleting=indexDeleteFile[i];
-            IDarrForDeleting.forEach(function(id){            
-                    BX24.callMethod(
-		              "disk.file.delete",
-		                  {
-			                 id: id
-		                  },
-		              function (result){
-			                 if (result.error()){
-                                console.error(result.error());                    
-                                $('#loading-pic').css({visibility:"hidden"});
-                                $('#delete-arr-length').html('Перезапустите программу');
-                                return false;
-                            }				    
-			                 else{
-                                console.dir(result.data());
-                                filesDeleted++;
-                                $('#delete-ready-info').html('Файлов удалено: '+filesDeleted);
-                            }				   
-		              }
-                    );            
-                });
-            if (i==index){
-                clearInterval(iID);
-                $('#loading-pic').css({visibility:"hidden"});                
-            }
-            return i++;
-        }, 25000);
     });
-}); 
+    $('#del').click(function(){
+        $('#loading-pic').css(styles);
+        fileDelRow=fileIDMatrix[f];        
+        if (fileDelRow!=undefined){
+            fileRowLength=fileDelRow.length;
+            fileDelRow.forEach(function(file,i){
+                BX24.callMethod("disk.file.delete",{id: file},function (result){
+			             if (result.error()&&i!=(fileRowLength-1)){                             
+                             console.log(file);
+                             $('#delete-ready-info').html('Файлов удалено: '+(f*matrixLength));
+                            }else if(result.error()){
+                                console.log(file);
+                                $('#delete-ready-info').html('Файлов удалено: '+(f*matrixLength));
+                                setTimeout(function(){
+                                        f++;
+                                        del.dispatchEvent(filesFound);
+                                },25000);
+                            } else{                                 
+                                $('#delete-ready-info').html('Файлов удалено: '+(f*matrixLength));
+                                if (i==(fileRowLength-1)){
+                                    setTimeout(function(){
+                                        f++;
+                                        del.dispatchEvent(filesFound);
+                                    },25000);
+                                }
+                            }				   
+		              });
+            });
+            
+        } else {             
+            $('#delete-ready-info').html('Удаление файлов завершено успешно.\nФайлов удалено: '+((f-1)*matrixLength+fileIDMatrix[(f-1)].length));          
+            $('#loading-pic').css({visibility:"hidden"});  
+        }
+    });
+});
+
+function MakeItMatrix(array, length){    
+     let matrix=[];
+     let index = Math.floor(array.length/length);     
+         for (var i=0; i<=index; i++){
+             matrix[i]=[];            
+             if (i==index){
+                 for(var j=0; j<length; j++){
+                     if(array[0]!=undefined){
+                         matrix[i][j]=array.pop();
+                     }                    
+                 }
+             } else {
+                 for(var j=0; j<length; j++){                
+                     matrix[i][j]=array.pop();                    
+                 } 
+             }
+     }
+     return matrix;
+ }
